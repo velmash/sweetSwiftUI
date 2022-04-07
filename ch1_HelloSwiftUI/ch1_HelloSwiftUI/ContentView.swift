582f995b9c3caec4b3ff3b14a391684226ee28d9
velmash <72960152+velmash@users.noreply.github.com>
2022-04-07 15:08:49 +0900

    :tada: ch1 init

---
+++ ch1_HelloSwiftUI/ch1_HelloSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ch1_HelloSwiftUI
+//
+//  Created by 윤형찬 on 2022/04/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ch1_HelloSwiftUI/ch1_HelloSwiftUI/ch1_HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ch1_HelloSwiftUIApp.swift
+//  ch1_HelloSwiftUI
+//
+//  Created by 윤형찬 on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct ch1_HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
